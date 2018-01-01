f3de5187ee522f58c779e8696dd3115ae474c629
Patrick dos Santos <patrickds@outlook.com>
2018-01-01 17:10:49 -0200

    Responds server requests as json

--- src/server/main.clj
+++ src/server/main.clj
@@ -2,18 +2,26 @@
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [GET defroutes]]
             [clj-http.client :as http]
-            [core.string-formatter :as string-formatter]
+            [cheshire.core :as json]
             [usecase.get-formatted-profile :refer :all]))
 
-(def get-profile-as-string
+(def get-profile-as-json
   (make-get-formatted-profile
    http/get
-   string-formatter/format-repositories))
+   json/generate-string))
 
-(defroutes server
+(defroutes handler
     (GET "/" [] "Hi pretty ( ͡° ͜ʖ ͡°)")
-    (GET "/:username" [username]
-      (get-profile-as-string username)))
+    (GET "/:username" [username] (get-profile-as-json username)))
+
+(defn json-content-type-middleware [handler]
+  (fn [request] 
+    (let [response (handler request)]
+      (assoc-in response [:headers "Content-Type"] "application/json"))))
+
+(def server
+  (-> handler
+      json-content-type-middleware))
 
 (defn -main [& args]
   (run-jetty server {:port 3000}))
--- test/unit/usecase/get_profile_as_string_test.clj
+++ test/unit/usecase/get_profile_as_string_test.clj
@@ -1,6 +1,7 @@
 (ns usecase.get-profile-as-string-test
   (:require [midje.sweet :refer :all]
             [mocks.http-client :as mock-http]
+            [cheshire.core :as json]
             [core.string-formatter :as string-formatter]
             [usecase.get-formatted-profile :refer [make-get-formatted-profile]]))
 
@@ -9,7 +10,17 @@
    mock-http/get
    string-formatter/format-repositories))
 
+(def get-profile-as-json
+  (make-get-formatted-profile
+   mock-http/get
+   json/generate-string))
+
 (fact "it returns a formatted profile"
       (get-profile-as-string "patrickds") =>
       (str "Name: android.experimental\tStars: 0\n"
            "Name: chat                \tStars: 1\n"))
+
+(fact "it returns a json formatted profile"
+      (get-profile-as-json "patrickds") =>
+      (str "[{\"name\":\"android.experimental\",\"stargazers_count\":0},"
+           "{\"name\":\"chat\",\"stargazers_count\":1}]"))
